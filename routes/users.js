e9fbf0a680970ec52f102f596cfcce5c1b3b7463
Sasha Czerniawski <sashacandoit@gmail.com>
2022-10-27 20:12:25 -0400

    get all users and get single user routes done

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,22 @@ const db = require("../db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
-const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+const { ensureLoggedIn, ensureAdmin, ensureCorrectUser } = require("../middleware/auth");
+const User = require("../models/user");
 
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+router.get('/', ensureLoggedIn, async (req, res, next) => {
+    try {
+        const users = await User.all();
+        return res.json({ users });
+    } catch (e) {
+        return next(e);
+    }
+});
 
 
 /** GET /:username - get detail of users.
@@ -19,6 +28,14 @@ const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+router.get('/:username', ensureCorrectUser, async (req, res, next) => {
+    try {
+        const user = await User.get(req.params.username);
+        return res.json({user})
+    } catch (e) {
+        return next(e);
+    }
+});
 
 
 /** GET /:username/to - get messages to user
